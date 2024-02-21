1a4ec40a8177dbf2b94aa600d3bb71fb7716be60
ninjadiego <diego3c34@gmail.com>
2024-02-20 23:30:30 -0500

    inicial

---
+++ NetCore6APIMySQL.DATA/Repositorio/CarRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore6APIMySQL.Data.repositorio
+{
+    public interface CarRepositorio
+    {
+        Task<Ienumerable><Car>> Getallcars();
+
+    }
+
+}
